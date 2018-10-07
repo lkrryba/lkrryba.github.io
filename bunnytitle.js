5da7ed55178ac1ce659ea05e38988e288436ad08
lkrryba <leanne.rybintsev@gmail.com>
2018-10-07 10:20:02 +0100

    footer etc

--- bunnytitle.js
+++ bunnytitle.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   /*$("button").prop("disabled", true);*/
   $("h1").addClass("animated bounce");
 
-  $("a").mouseenter(function() {
-    $("a").css("color", "green");
+  $("#clickhere").mouseenter(function() {
+    $("#clickhere").css("color", "green");
   });
 
-  $("a").mouseout(function() {
-    $("a").css("color", "blue");
+  $("#clickhere").mouseout(function() {
+    $("#clickhere").css("color", "blue");
   });
 
   $('[data-toggle="tooltip"]').tooltip();
